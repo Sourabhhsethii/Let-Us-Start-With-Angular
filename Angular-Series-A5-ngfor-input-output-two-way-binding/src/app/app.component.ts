3e06d42176fa7737657fac1e89860a6e991f7eeb
SOURABHHSETHII <sourabhhsethii@yahoo.co.in>
2020-05-15 18:54:02 +0530

    Adding A4,A5 and A6 articles of this series.

---
+++ Angular-Series-A5-ngfor-input-output-two-way-binding/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular-Series-A5-ngfor-input-output-two-way-binding';
+}
